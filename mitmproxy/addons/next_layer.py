3602aba6b85220cac275dbeab3bfb9c5f9dabdfd
Jaroslav Lobačevski <jarlob@github.com>
2024-07-05 14:00:08 +0200

    Update next_layer.py

--- mitmproxy/addons/next_layer.py
+++ mitmproxy/addons/next_layer.py
@@ -231,7 +231,10 @@ class NextLayer:
         ) and client_hello.sni:
             hostnames.append(f"{client_hello.sni}:{port}")
 
-        f"_ignore_connection: hostnames {hostnames}"
+        logger.info(
+            f"_ignore_connection: hostnames {hostnames}"
+        )
+
         if not hostnames:
             return False
 
